b02d632af4fbcb0c6315d6c45c0e54ceafb7897a
damithrm <damithrmdr@gamil.com>
2026-06-24 09:05:50 +0530

    deploy with docker to EC2 instance

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello World From AWS EC2 1 Test CI/CD</h1>
+      <h1>Hello World From AWS EC2 Test CI/CD 2</h1>
     </div>
   );
 }
